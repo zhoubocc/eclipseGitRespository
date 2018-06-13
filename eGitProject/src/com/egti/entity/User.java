1bf84f57e380162f9e943d60e04c560504fbfe90
zhoubocc <332467500@qq.com>
2018-06-13 17:31:40 +0800

    create project

---
+++ eGitProject/src/com/egti/entity/User.java
@@ -0,0 +1,30 @@
+package com.egti.entity;
+
+public class User {
+	
+	private int id;
+	private String userName;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+
+}
